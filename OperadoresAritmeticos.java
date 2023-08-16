26b39624f43f4563686f2d7cfbe10bcb9c38ac01
David <142182549+dmaiato@users.noreply.github.com>
2023-08-16 19:35:00 -0300

    Add files via upload

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+public class OperadoresAritmeticos {
+  public static void main(String[] args) {
+    
+    int a, b;
+
+    a = 10;
+    b = 3;
+
+    int soma, mult, div, dif, resto;
+    soma = a + b;
+    soma = soma - b;
+
+    System.out.println(soma);
+  }
+}
